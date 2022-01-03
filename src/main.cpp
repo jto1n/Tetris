0e134aead894d1469fa3a976b8550b30e2f0fb95
jto1n <j.l.jones013@gmail.com>
2022-01-03 22:06:59 +0000

    got opengl to produce a triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,156 @@
-// Example program:
-// Using SDL2 to create an application window
-
-#include <SDL2/SDL.h>
 #include <stdio.h>
-#include <SDL2/SDL_opengl.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <iostream>
 
-int main(int argc, char* argv[]) {
+/* A simple function that prints a message, the error code returned by SDL,
+ * and quits the application */
+void sdldie(const char *msg)
+{
+    printf("%s: %s\n", msg, SDL_GetError());
+    SDL_Quit();
+    exit(1);
+}
 
-    SDL_Window *window;                    // Declare a pointer
 
-    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
-    // (void)argc;
-    // (void)argv;
-    // Create an application window with the following settings:
-    window = SDL_CreateWindow(
-        "An SDL2 window",                  // window title
-        SDL_WINDOWPOS_UNDEFINED,           // initial x position
-        SDL_WINDOWPOS_UNDEFINED,           // initial y position
-        640,                               // width, in pixels
-        480,                               // height, in pixels
-        SDL_WINDOW_OPENGL                  // flags - see below
-    );
+void checkSDLError(int line = -1)
+{
+#ifndef NDEBUG
+	const char *error = SDL_GetError();
+	if (*error != '\0')
+	{
+		printf("SDL Error: %s\n", error);
+		if (line != -1)
+			printf(" + line: %i\n", line);
+		SDL_ClearError();
+	}
+#endif
+}
+ 
+ float vert[] = 
+    {
+        -0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+    };
 
-    // Check that the window was successfully created
-    if (window == NULL) {
-        // In the case that the window could not be made...
-        printf("Could not create window: %s\n", SDL_GetError());
-        return 1;
-    }
+const char* vsSource = 
+"in vec3 position;"
+"void main("
+"{"
+"gl_Position = vec4(poisition.x, poisition.y, poisition.z, 1.0)"
+"}";
 
-    // The window is open: could enter program loop here (see SDL_PollEvent())
+const char* fsSource = 
+"out vec4 color;"
+"void main()"
+"{"
+"color = vec4(1.0f, 0.0f, 0.0f, 1.0f);"
+"}";
 
-    SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+/* Our program's entry point */
+int main()
+{
+    SDL_Window *window; /* Our window handle */
+    SDL_GLContext context; /* Our opengl context handle */
 
-    // Close and destroy the window
+
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) /* Initialize SDL's Video subsystem */
+        sdldie("Unable to initialize SDL"); /* Or die on error */
+
+    /* Request opengl 4.6 context.
+     * SDL doesn't have the ability to choose which profile at this time of writing,
+     * but it should default to the core profile */
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+
+    window = SDL_CreateWindow("Tetris", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 400, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+    
+
+    context = SDL_GL_CreateContext(window);
+
+
+    GLenum err = glewInit();
+if (err != GLEW_OK)
+{
+    std::cout << "error: " << glewGetErrorString(err) <<  "\n";
+    SDL_GL_DeleteContext(context);
     SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 1;
+}
 
-    // Clean up
+    unsigned int vs;
+    unsigned int fs;
+    unsigned int shader;
+
+    vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vsSource, NULL);
+    glCompileShader(vs);
+
+
+    fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fsSource, NULL);
+    glCompileShader(fs);
+
+    shader = glCreateProgram();
+    glAttachShader(shader, vs);
+    glAttachShader(shader, fs);
+    glLinkProgram(shader);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vert), vert, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    SDL_Event event;
+    bool running = true;
+
+    while(running)
+    {
+        while(SDL_PollEvent(&event))
+        {
+            switch(event.type)
+            {
+                case SDL_QUIT:
+                    running = false;
+                    break;
+                default:
+                    break;
+
+            }
+
+        }
+
+        /* Clear our buffer with a red background */
+    glClearColor ( 1.0, 0.0, 0.0, 1.0 );
+
+    glClear ( GL_COLOR_BUFFER_BIT );
+
+    glUseProgram(shader);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
+    // /* Swap our back buffer to the front */
+    SDL_GL_SwapWindow(window);
+    }
+    SDL_GL_DeleteContext(context);
+    SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
+           
